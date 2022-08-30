e5fc3ec96f92a85226c9eac4eb63babfd1749e2b
Miguel Teheran <miguelteheran@gmail.com>
2022-08-30 16:53:56 -0500

    principio kiss

--- Program.cs
+++ Program.cs
@@ -57,14 +57,11 @@ namespace ToDo
                 string taskNumberToDelete = Console.ReadLine();
                 // Remove one position
                 int indexToRemove = Convert.ToInt32(taskNumberToDelete) - 1;
-                if (indexToRemove > -1)
+                if (indexToRemove > -1 && TaskList.Count > 0)
                 {
-                    if (TaskList.Count > 0)
-                    {
                         string taskToRemove = TaskList[indexToRemove];
                         TaskList.RemoveAt(indexToRemove);
                         Console.WriteLine("Tarea " + taskToRemove + " eliminada");
-                    }
                 }
             }
             catch (Exception)
@@ -95,10 +92,9 @@ namespace ToDo
             else
             {
                 Console.WriteLine("----------------------------------------");
-                for (int i = 0; i < TaskList.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
-                }
+                var indexTask=1;
+                TaskList.ForEach(p=> Console.WriteLine(indexTask++ + ". " + p));
+                
                 Console.WriteLine("----------------------------------------");
             }
         }
