53e504d4fd41ea9534da76e3e92443f95fca5890
Miguel Teheran <miguelteheran@gmail.com>
2022-08-30 17:47:39 -0500

    mejoras Interpolación de cadenas, inicializador de propiedades y operador condicional null

--- Program.cs
+++ Program.cs
@@ -5,11 +5,10 @@ namespace ToDo
 {
     internal class Program
     {
-        public static List<string> TaskList { get; set; }
+        public static List<string> TaskList { get; set; } =  new List<string>();
 
         static void Main(string[] args)
         {
-            TaskList = new List<string>();
             int menuSelected = 0;
             do
             {
@@ -93,17 +92,17 @@ namespace ToDo
 
         public static void ShowMenuTaskList()
         {
-            if (TaskList == null || TaskList.Count == 0)
-            {
-                Console.WriteLine("No hay tareas por realizar");
-            } 
-            else
+            if (TaskList?.Count > 0)
             {
                 Console.WriteLine("----------------------------------------");
                 var indexTask=1;
-                TaskList.ForEach(p=> Console.WriteLine(indexTask++ + ". " + p));
+                TaskList.ForEach(p=> Console.WriteLine($"{indexTask++} . {p}"));
                 
                 Console.WriteLine("----------------------------------------");
+            } 
+            else
+            {
+                Console.WriteLine("No hay tareas por realizar");
             }
         }
     }
