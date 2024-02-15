654ae2e1180364149103de14491abafc0114bfb0
chanelp <mariannissanchez27@gmail.com>
2024-02-15 16:30:54 -0400

    Code Smells - Magic Numbers

--- Program.cs
+++ Program.cs
@@ -14,19 +14,19 @@ namespace ToDo
             do
             {
                 menuSelected = ShowMainMenu();
-                if (menuSelected == 1)
+                if ((Menu)menuSelected == Menu.Add)
                 {
                     ShowMenuAdd();
                 }
-                else if (menuSelected == 2)
+                else if ((Menu)menuSelected == Menu.Remove)
                 {
                     ShowMenuRemove();
                 }
-                else if (menuSelected == 3)
+                else if ((Menu)menuSelected == Menu.List)
                 {
                     ShowMenuTaskList();
                 }
-            } while (menuSelected != 4);
+            } while ((Menu)menuSelected != Menu.Exit);
         }
         /// <summary>
         /// Show the main menu 
@@ -106,5 +106,13 @@ namespace ToDo
                 Console.WriteLine("----------------------------------------");
             }
         }
+
+        public enum Menu{
+            Add = 1,
+            Remove = 2,
+            List = 3,
+            Exit = 4
+        }
+
     }
 }
